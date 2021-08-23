429df2a75df62d759dea2e2a6ac16b022a86088e
superswan <ohm@airmail.cc>
2021-08-22 20:22:13 -0400

    Make sections collapsible

--- static/js/site.js
+++ static/js/site.js
@@ -1,8 +1,26 @@
+// Link Counter
 function displayLinks() {
-    var siteList = document.getElementById("sites");
-    var linkList = siteList.getElementsByTagName("a");
+    var siteList = document.getElementsByClassName("links");
 
-    console.log(linkList.length);
+    for (i = 0; i < siteList.length; i++) {
+        linkList = siteList[i].getElementsByTagName("a");
+    }
 
     var linkCount = document.getElementById("link-count").innerHTML = linkList.length;
+}
+
+// Collapse 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
