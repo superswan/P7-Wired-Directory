bae95852867ed4146ae9ba57293b569d2d491a25
Masami <masami@protocol7.co>
2021-09-16 22:15:20 -0400

    fix search

--- static/js/search.js
+++ static/js/search.js
@@ -2,17 +2,27 @@ function search() {
     var input, filter, ul, li, span, a, i, txtValue, title;
     input = document.getElementById("srch");
     filter = input.value.toLowerCase();
-    ul = document.getElementById("sites");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        span = li[i].getElementsByTagName("span")[0];    
-        txtValue = span.textContent || span.innerText;
-        a = span.firstChild;
-        title = a.title;
-        if ((txtValue.toLowerCase().indexOf(filter) > -1) || (title.toLowerCase().indexOf(filter) > -1)) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+
+    var siteList = document.getElementsByClassName("links").item(0);
+    ul = siteList.getElementsByClassName("sites");
+    //li = ul.getElementsByTagName("li");
+
+    console.log(ul.length)
+    for (i = 0; i < ul.length; i++) {
+        topics = ul.item(i);
+        li = topics.getElementsByTagName("li");
+        console.log(li);
+        
+        for (j = 0; j < li.length; j++) {
+            span = li[j].getElementsByTagName("span")[0];
+            txtValue = span.textContent || span.innerText;
+            a = span.firstChild;
+            title = a.title;
+            if ((txtValue.toLowerCase().indexOf(filter) > -1) || (title.toLowerCase().indexOf(filter) > -1)) {
+                li[j].style.display = "";
+            } else {
+                li[j].style.display = "none";
+            }
         }
     }
-    }
+}
