73fe35e08743b4f5f390be254e2cc97daf2cef94
Masami <masami@protocol7.co>
2021-09-16 20:28:10 -0400

    Minor tweaks

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,7 @@
 function search() {
     var input, filter, ul, li, span, a, i, txtValue, title;
     input = document.getElementById("srch");
-    filter = input.value.toUpperCase();
+    filter = input.value.toLowerCase();
     ul = document.getElementById("sites");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
@@ -9,7 +9,7 @@ function search() {
         txtValue = span.textContent || span.innerText;
         a = span.firstChild;
         title = a.title;
-        if ((txtValue.toUpperCase().indexOf(filter) > -1) || (title.toUpperCase().indexOf(filter) > -1)) {
+        if ((txtValue.toLowerCase().indexOf(filter) > -1) || (title.toLowerCase().indexOf(filter) > -1)) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
