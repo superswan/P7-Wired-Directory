a2b59e2dbb4ab9f5d24c65a9fb324d7b38f3c3b5
Masami <masami@protocol7.co>
2021-10-02 02:09:47 -0400

    update text file generator

--- genTextFile.py
+++ genTextFile.py
@@ -1,17 +1,37 @@
 """
 Generates a textfile from static.html located in /public_html/ directory
-Plan to implement better parsing/formatting for the final output.
 
 Usage:
 python genTextFile.py > wired.txt
 """
 from bs4 import BeautifulSoup
 
-static_html = "public_html/static.html"
+source_file = "public_html/index.html"
+with open(source_file) as html:
+    soup = BeautifulSoup(html, "html.parser")
 
-with open(static_html) as html:
-    soup = BeautifulSoup(html, 'html.parser')
+categories = [] 
+for button in soup.find_all("button", {"class": "collapsible"}):
+    category = button.text
+    categories.append(category)
 
-print(soup.get_text())
+categoryDivs = []
+for content in soup.find_all("div", {"class": "content"}):
+    categoryDivs.append(content)
 
+for i, category in enumerate(categories):
+    div = categoryDivs[i]
+    subCatList = div.find_all('ul')
+    print(f"=== {category} ===")
+    for j, ul in enumerate(subCatList):
+        subTitle = ul.find('h3')
+        subTitle.extract()
+        subTitle = subTitle.text
+        if subTitle.endswith(" "):
+            subTitle = subTitle[:-1]
+        if subTitle.startswith(' '):
+            subTitle = subTitle[1:]
+        subTitle = subTitle.strip(":")
+        print(subTitle)
+        print(subCatList[j].text)
 
