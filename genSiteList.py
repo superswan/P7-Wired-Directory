835a0e8bde122557b6052c8a7065c53276cacb06
Masami <masami@protocol7.co>
2021-10-02 12:00:55 -0400

    Complete JSON output

--- genSiteList.py
+++ genSiteList.py
@@ -1,11 +1,13 @@
 """
 Generates a JSON file from static.html located in /public_html/ directory
 
-Output is also used to generate the text file
+Output is also used to generate the text file. This script can be deprecated
+once the JSON file is populated.
+
+For text file:
 python genTextFile.py > wired.txt
 """
-
-import json
+import jsonpickle
 from bs4 import BeautifulSoup
 
 class Category:
@@ -16,7 +18,7 @@ class Category:
 class SubCategory:
     def __init__(self, title, subcategoryList):
         self.title = title
-        self.subcategoryList = subcategoryList
+        self.sites = subcategoryList
 
 class Site:
     def __init__(self, title, url, description, tags):
@@ -38,21 +40,25 @@ categoryDivs = []
 for content in soup.find_all("div", {"class": "content"}):
     categoryDivs.append(content)
 
-for i, category in enumerate(categories):
+categoryObjs = []
+
+# Makes a list of objects to be dumped into JSON
+for i, category in enumerate(categories): # Main section categories
     div = categoryDivs[i]
     subCatList = div.find_all('ul')
     print(f"=== {category} ===\n")
-    for j, ul in enumerate(subCatList):
+
+    subCatObjs = []
+    for j, ul in enumerate(subCatList): # Get subcategories and associated sites
         subTitle = ul.find('h3')
         subTitle.extract()
         subTitle = subTitle.text
-        if subTitle.endswith(" "):
-            subTitle = subTitle[:-1]
-        if subTitle.startswith(' '):
-            subTitle = subTitle[1:]
+        subTitle = subTitle.rstrip().lstrip()
         subTitle = subTitle.strip(":")
         print(f"[{subTitle}]\n")
-        site_info = subCatList[j].find_all('span')
+        site_info = subCatList[j].find_all('span') #Subcategory ul element
+
+        siteObjs = []
         for site in site_info:
             if len(site.text) != 0:
                 url = site.find('a', href=True)
@@ -62,5 +68,16 @@ for i, category in enumerate(categories):
                 description = site.text
                 description = description.replace("-", '')
                 description = description.lstrip()
-                print(f"{link} - {description}")
+                print(f"{title}:{link} - {description}")
+                tags = []
+                site = Site(title, link, description, tags)
+                siteObjs.append(site)
+        subcat = SubCategory(subTitle, siteObjs)
+        subCatObjs.append(subcat)
         print("\n")
+    categoryObj=Category(category, subCatObjs)
+    categoryObjs.append(categoryObj)
+
+json_data = jsonpickle.encode(categoryObjs, unpicklable=False)
+with open('site-list.json', 'w') as outputf:
+    print(json_data, file=outputf)
--- linkstatus.py
+++ linkstatus.py
@@ -5,7 +5,7 @@ Outputs active links to active.txt
 from bs4 import BeautifulSoup
 import requests
 
-static_html = "public_html/static.html"
+static_html = "public_html/static.html" #pull from JSON in future
 
 with open(static_html) as html:
     soup = BeautifulSoup(html, 'html.parser')
@@ -14,6 +14,7 @@ active_links = []
 for tag in soup.find_all('a', href=True):
     url = tag['href']
     try:
+        #add timeout and headers, checks for 404 as opposed to pinging server
         req = requests.get(url)
         if req.ok:
             print(f"{url}:[LIVE]")
